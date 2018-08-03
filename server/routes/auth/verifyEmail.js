3b8bf58076c8edfbb8c677bd94edb519edeed8a9
L1lith <lily@lillith.pw>
2018-08-03 16:50:30 -0600

    cleanup verifyemail

--- server/routes/auth/verifyEmail.js
+++ server/routes/auth/verifyEmail.js
@@ -2,12 +2,13 @@ const {sandhandsExpress} = require('sandhands')
 
 function verifyEmail(router, {middleware}) {
   router.get('/verifyEmail', middleware.authenticate({getUser: true, requireVerification: false}), (req, res, next) => {
-    if (req.user.verified === true) return res.status(400).send('Already Verified')
+    const {user} = req
+    if (user.verified === true) return res.status(400).send('Already Verified')
     const {randomBytes} = require('crypto')
     randomBytes(8, (err, buf) => {
       if (err) return next(err)
-      const verificationCode = req.user.verificationCode = buf.toString('base64')
-      req.user.save(err => {
+      const verificationCode = user.verificationCode = buf.toString('base64')
+      user.save(err => {
         if (err) return next(err)
         res.sendStatus(200)
         // Email the verification code here
@@ -15,20 +16,23 @@ function verifyEmail(router, {middleware}) {
     })
   })
   router.post('/verifyEmail', sandhandsExpress({verificationCode: String}), middleware.authenticate({getUser: true, requireVerification: false}), (req, res) => {
-    if (typeof req.user.verificationCode != 'string') return res.sendStatus(500)
-    if (req.body.verificationCode === req.user.verificationCode) {
+    const {user} = req
+    if (typeof user.verificationCode != 'string') return res.sendStatus(500)
+    if (req.body.verificationCode === user.verificationCode) {
       // Passed Verification
-      req.user.verified = true
-      req.user.verificationCode = undefined
-      req.user.save(err => {
+      user.verified = true
+      user.verificationCode = undefined
+      user.save(err => {
         if (err) return next(err)
         res.sendStatus(200)
       })
     } else {
       // Failed Verification
       res.status(401).send('Invalid Verification Code')
-      req.user.verificationCode = undefined
-      req.user.save()
+      user.verificationCode = undefined
+      user.save(err => {
+        if (err) console.log(err)
+      })
     }
   })
 }
