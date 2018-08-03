b879358a8d41be05265dd7ae01e5402658ffa033
L1lith <lily@lillith.pw>
2018-08-03 12:30:37 -0600

    finish up authentication

--- server/routes/auth/logout.js
+++ server/routes/auth/logout.js
@@ -1,8 +1,11 @@
 function logout(router, {functions}) {
-  router.get('/logout', functions.middleware.authenticate(), (req,res) => {
-    res.clearCookie("session")
-    res.clearCookie("username")
-    res.sendStatus(200)
+  router.get('/logout', functions.middleware.authenticate(), (req, res, next) => {
+    req.session.remove(err => {
+      if (err) return next(err)
+      res.clearCookie("session")
+      res.clearCookie("username")
+      res.sendStatus(200)
+    })
   })
 }
 
