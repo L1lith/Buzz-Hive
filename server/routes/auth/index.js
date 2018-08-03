1f639535695907accab82534232ad42252cda1c6
L1lith <lily@lillith.pw>
2018-08-03 14:11:30 -0600

    update rate limit options

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -3,8 +3,8 @@ const RateLimit = require('express-rate-limit')
 const authLimit = new RateLimit({
   windowMs: 10 * 24 * 60 * 60 * 1000, // 10 days
   max: 100,
-  delayAfter: 1, // After First Request
-  delayMs: 2 * 1000 // Delay 2 Seconds,
+  delayAfter: 15, // After 15th Request
+  delayMs: 2 * 60 * 60 * 1000 // Delay 2 hours
   message: "Too many requests to the authentication API, please try again later."
 })
 
