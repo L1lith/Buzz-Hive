0f2f83046927b8236e57952f257963acf3c9e766
L1lith <lily@lillith.pw>
2018-08-03 10:27:02 -0600

    work on auth

--- server/routes/auth/login.js
+++ server/routes/auth/login.js
@@ -1,10 +1,14 @@
 const {sandhandsExpress} = require('sandhands')
+const basicAuth = require('express-basic-auth')
 
-function login(router) {
+function login(router, {models}) {
   router.get('/login', sandhandsExpress({
     username: 'username',
     password: 'password'
   }))
+  router.get('/login', basicAuth({authorizer: (username, password) => {
+
+  }}))
   router.get('/login', (req, res) => {
     console.log(req.body)
   })
