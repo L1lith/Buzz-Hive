a2a2b7a308ca07fd6a97e269846734ce89e436fd
L1lith <lily@lillith.pw>
2018-08-03 14:06:29 -0600

    create auth rate limit

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,5 +1,15 @@
+const RateLimit = require('express-rate-limit')
+
+const authLimit = new RateLimit({
+  windowMs: 10 * 24 * 60 * 60 * 1000, // 10 days
+  max: 100,
+  delayAfter: 1, // After First Request
+  delayMs: 2 * 1000 // Delay 2 Seconds,
+  message: "Too many requests to the authentication API, please try again later."
+})
+
 function auth(router) {
-  
+  router.use(authLimit)
 }
 
 module.exports = auth
