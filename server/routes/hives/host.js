f1b83e82b12283c3262c5427eae017e98cf25c44
L1lith <lily@lillith.pw>
2018-08-04 08:48:31 -0600

    remove quarry-dns

--- server/routes/hives/host.js
+++ server/routes/hives/host.js
@@ -1,12 +1,5 @@
-const {launchQuarry} = require('../../quarry')
-
 async function hostHive(router, {middleware}) {
-  const {quarry, configuration, records} = await launchQuarry()
-  console.log({quarry, configuration, records})
-  router.get('/host', middleware.authenticate({getUser: true}),(req, res) => {
-    const ip = req.ip.split(':')[0]
-
-  })
+  
 }
 
 module.exports = hostHive
