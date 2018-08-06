8a6a47beae42883270b4ec0d96ebb97ab7969ff1
L1lith <lily@lillith.pw>
2018-08-06 12:08:09 -0700

    update vapid key fetch function

--- client/functions/fetchVapidKey.js
+++ client/functions/fetchVapidKey.js
@@ -1,5 +1,5 @@
 async function fetchVapidKey() {
-  const vapidKey = await (await fetch()).text()
+  const vapidKey = await (await fetch('/vapidkey')).text()
   const modified = vapidKey === localStorage.vapidKey
   localStorage.vapidKey = vapidKey
   return {vapidKey, modified}
