75801535813f85511101e58f72a4f959d6578746
L1lith <lily@lillith.pw>
2018-08-06 15:30:36 -0700

    create fetchIfModified

---
+++ client/functions/fetchIfModified.js
@@ -0,0 +1,37 @@
+async function fetchIfModified(url, fetchOptions={}) {
+  const cachePath = 'cache/' + url
+  let cache = localStorage[cachePath]
+  if (cache) {
+    cache = JSON.parse(cache)
+    const {lastFetched, lastValue} = cache
+    const response = await fetch(url, {...fetchOptions, headers: {"If-Modified-Since": lastFetched}})
+    if (response.status === 200) {
+      const value = await response.text()
+      cacheResponse(cachePath, value)
+      return {value, modifed: true, response}
+    } else if (response.status === 304) {
+      return {value: lastValue, modified: false, response}
+    } else {
+      throw new Error(`Unexpected Status Code "${response.status}"`)
+    }
+  } else {
+    const response = await fetch(url)
+    const value = await response.text()
+    cacheResponse(cachePath, value)
+    if (response.status === 200) {
+      return {value, modified: true, response}
+    } else {
+      throw new Error(`Unexpected Status Code "${response.status}"`)
+    }
+  }
+}
+
+function cacheResponse(cachePath, value) {
+  let cache = {
+    lastValue: value,
+    lastFetched: (new Date()).toUTCString()
+  }
+  localStorage[cachePath] = JSON.stringify(cache)
+}
+
+module.exports = fetchVapidKey
--- client/functions/fetchVapidKey.js
+++
@@ -1,8 +0,0 @@
-async function fetchVapidKey() {
-  const vapidKey = await (await fetch('/vapidkey')).text()
-  const modified = vapidKey !== localStorage.vapidKey
-  localStorage.vapidKey = vapidKey
-  return {vapidKey, modified}
-}
-
-module.exports = fetchVapidKey
