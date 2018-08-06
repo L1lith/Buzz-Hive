ecd50d91d35a865dd5b5523f600641641c7d1268
L1lith <lily@lillith.pw>
2018-08-06 15:41:14 -0700

    fix vapidkey route path

--- server/routes/vapidKey.js
+++ server/routes/vapidKey.js
@@ -1,6 +1,6 @@
 function vapidKey(router, {functions, vapidKeys}) {
   const {publicKey, modified} = vapidKeys
-  router.get('/vapidKey', functions.serveStaticString(publicKey, modified))
+  router.get('/vapidkey', functions.serveStaticString(publicKey, modified))
 }
 
 module.exports = vapidKey
