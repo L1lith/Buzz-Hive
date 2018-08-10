27d67655a6439008bbf9c2fe4541e0b7b3ebfb50
L1lith <lily@lillith.pw>
2018-08-10 16:13:23 -0600

    fix update endpoint

--- server/routes/devices/update.js
+++ server/routes/devices/update.js
@@ -1,18 +1,19 @@
 const {sandhandsExpress} = require('sandhands')
 
-function updateDevice(router, {middleware}) {
+function updateDevice(router, {functions, middleware}) {
+  const {validPushURL} = functions
   router.patch('/update', middleware.authenticate({getUser: true}), sandhandsExpress({
     pushURL: {_: String, optional: true},
-    deviceName: {_: String, optional: true},
-    newDeviceName: {_: String, optional: true}
-  }), (req, res) => {
-    const {user} = req
-    const {devices} = user
-    const targetDevice = devices.filter(device => device.name === req.body.deviceName)[0]
-    if (!targetDevice) return res.status(404).send('Device Not Found')
-    if (req.body.hasOwnProperty('newDeviceName')) targetDevice.name = newDeviceName
-    if (req.body.hasOwnProperty('pushURL')) targetDevice.name = req.body.newDeviceName
-    user.save(err => {
+    deviceName: {_: String, optional: true}
+  }), middleware.getDevices({singleDevice: true}), (req, res) => {
+    const {device} = req
+
+    if (req.body.hasOwnProperty('deviceName')) device.name = req.body.deviceName
+    if (req.body.hasOwnProperty('pushURL')) {
+      if (!validPushURL(req.body.pushURL)) return res.sendStatus(400)
+      device.pushURL = req.body.pushURL
+    }
+    device.save(err => {
       if (err) return next(err)
       res.sendStatus(200)
     })
