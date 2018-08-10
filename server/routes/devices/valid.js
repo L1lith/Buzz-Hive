56bb6acd26c50c304be3409a4da1ffe6b43ab30a
L1lith <lily@lillith.pw>
2018-08-10 15:35:41 -0600

    remove console logs

--- server/routes/devices/valid.js
+++ server/routes/devices/valid.js
@@ -4,8 +4,6 @@ function validDevice(router, {middleware}) {
   router.post('/valid', sandhandsExpress({
     pushURL: String
   }), middleware.authenticate({getUser: true}), middleware.getDevices({singleDevice: true}), (req, res) => {
-    console.log('req url',req.body.pushURL, '\n device url', req.device.pushURL)
-    console.log('matched:', req.body.pushURL === req.device.pushURL)
     if (req.body.pushURL === req.device.pushURL) {
       res.sendStatus(200)
     } else {
