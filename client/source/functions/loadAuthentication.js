9ea3e213451d1802fc8752a68f38f6b466b2b40b
L1lith <lily@lillith.pw>
2018-08-12 16:53:57 -0600

    create getting login status

--- client/source/functions/loadAuthentication.js
+++ client/source/functions/loadAuthentication.js
@@ -1,5 +1,13 @@
-function loadAuthentication(store) {
-  
+async function loadAuthentication(store) {
+  let response
+  try {
+    response = await fetch('/auth/valid', {statusRange: 200})
+  } catch(err) {
+    return
+  }
+  const {username} = await response.json()
+  store.auth.loggedIn = true
+  store.auth.username = username
 }
 
 export default loadAuthentication
