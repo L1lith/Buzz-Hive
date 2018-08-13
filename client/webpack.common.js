43e2142a742f6c73b27e62a5f59cb455a5216f5f
L1lith <lily@lillith.pw>
2018-08-12 20:11:37 -0600

    add less loaders

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -17,6 +17,17 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader' // translates CSS into CommonJS
+        }, {
+          loader: 'less-loader' // compiles Less to CSS
+        }]
       }
     ]
   },
