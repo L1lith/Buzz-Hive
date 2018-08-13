c0fbea791e6d71e934be749098b802701b1e9dff
L1lith <lily@lillith.pw>
2018-08-13 01:54:32 -0600

    work on header auth state

--- client/source/components/header.js
+++ client/source/components/header.js
@@ -1,12 +1,27 @@
 import {connect} from 'jabr-react'
+import autoBind from 'auto-bind'
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    autoBind(this)
+  }
   render() {
+    return (
+      <header className="main">
+        <h1 className="site-title">Buzz Hive</h1>
+        <div className="auth">{this.renderAuth()}</div>
+      </header>
+    )
+  }
+  renderAuth() {
     const {loggedIn, username} = this.props.store.auth
-    if (loggedIn === true) {
+    if (loggedIn === null) {
+      return null
+    } else if (loggedIn === true) {
       return <p>Welcome {username}</p>
-    } else if (loggedIn === false){
-      return <p>Please Login</p>
+    } else if (loggedIn === false) {
+      return <p>Login</p>
     } else {
       return null
     }
