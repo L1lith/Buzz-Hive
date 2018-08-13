74f9bace8632429dd478035b5102c268b578eb81
L1lith <lily@lillith.pw>
2018-08-12 19:41:32 -0600

    remove log statement

--- client/source/components/header.js
+++ client/source/components/header.js
@@ -3,7 +3,6 @@ import {connect} from 'jabr-react'
 class Header extends React.Component {
   render() {
     const {loggedIn, username} = this.props.store.auth
-    console.log(loggedIn, username)
     if (loggedIn === true) {
       return <p>Welcome {username}</p>
     } else if (loggedIn === false){
