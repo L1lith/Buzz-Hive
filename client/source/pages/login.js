20cda1238a0558c43ed1d47f7e30e58522b6a2a6
L1lith <lily@lillith.pw>
2018-08-26 17:46:29 -0600

    work on login form

--- client/source/pages/login.js
+++ client/source/pages/login.js
@@ -13,19 +13,28 @@ class Login extends React.Component {
     this.state = {successful: null, error: null}
   }
   async submit({username, password}) {
-    const response = await fetch('/auth/login', {headers: {Authorization: "Basic "+btoa(`${username}:${password}`)}})
+    let response
+    try {
+      response = await fetch('/auth/login', {headers: {Authorization: "Basic "+btoa(`${username}:${password}`)}})
+    } catch (error) {
+      return this.setState({error: "Network Error"})
+    }
     if (Math.floor(response.status / 100) === 2) {
       const {auth} = this.props.store
       this.props.store.auth = {loggedIn: true, name: (await response.json()).name}
       this.setState({successful: true})
     } else {
-      this.setState({successful: false, error: mapResponseTexts.hasOwnProperty(response.statusText) ? mapResponseTexts[response.statusText] : response.statusText || null})
+      this.setState({error: mapResponseTexts.hasOwnProperty(response.statusText) ? mapResponseTexts[response.statusText] : response.statusText || null})
     }
   }
   render() {
     if (this.state.successful === true ) return <Redirect to="/"/>
+    console.log(this.state.error)
     return (
-      <Form onSubmit={this.submit} onError={console.log}>
+      <Form onSubmit={this.submit} onError={error => this.setState({error})}>
+        {this.state.error ? (
+          <span className="error">{this.state.error}</span>
+        ) : null}
         <input name="username"/>
         <input name="password" type="password"/>
         <input type="submit"/>
