3a263d99575eccdc7d84f16006d1a30f1a5eaa61
L1lith <lily@lillith.pw>
2018-09-02 17:18:09 -0600

    start configuring build env

---
+++ client/webpack.production.js
@@ -0,0 +1,15 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  plugins: [new UglifyJsPlugin({
+    uglifyOptions: {
+      compress: {
+        warnings: false,
+        drop_console: true
+      },
+      output: {
+        comments: false
+      }
+    }
+  })]
+}
